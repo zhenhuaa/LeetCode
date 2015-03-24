4619f0e4e13d2f7dce34332ca38636b4159164a9
zhenhua.wang <wangzhenhuaa@gmail.com>
2015-03-24 22:03:22 +0800

    best-time-to-buy-and-sell-stock-ii.js rewrite

--- best-time-to-buy-and-sell-stock-ii.js
+++ best-time-to-buy-and-sell-stock-ii.js
@@ -3,36 +3,9 @@
  *@return {number}
  */
 var maxProfit = function(prices) {
-  if(prices.length === 0) return 0;
-  var findMin = true;
-  var min = prices[0];
-  var max;
-  var maxProfitRes = 0;
-  var i, e;
-  for(i = 0; i < prices.length; i++) {
-    e = prices[i];
-    if(findMin) {
-      if(e < min) {
-        min = e;
-      } 
-      if (e > min) {
-        max = e;
-        findMin = false;
-      }
-    } else {
-      if( e > max) {
-        max = e;
-      }
-      if( e < max) {
-        maxProfitRes += (max - min);
-        min = e;
-        max = null;
-        findMin = true;
-      }
-    }
-    if(max &&  i === prices.length - 1) {
-      maxProfitRes += (max - min);
-    }
-  }
-  return maxProfitRes;
+  return prices.length < 2 ? 0 :prices.map(function (e, i, arr) {
+    return i ? Math.max(0, e - arr[i-1]) : 0;
+  }).reduce(function (a, b) {
+    return a + b;
+  });
 };
