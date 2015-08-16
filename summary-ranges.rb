10172f9aa7041f3a555ecceefcc5f5c441b82d52
zhenhua.wang <wangzhenhuaa@gmail.com>
2015-08-17 01:58:31 +0800

    add summary-ranges.rb

---
+++ summary-ranges.rb
@@ -0,0 +1,16 @@
+# @param {Integer[]} nums
+# @return {String[]}
+def summary_ranges(nums)
+  return nil if nums.empty?
+  ranges = [[nums.first, nums.first]]
+
+  nums.each do |num|
+    range = ranges.last
+    if num - range.last > 1
+      ranges << [num, num]
+    else
+      range[-1] = num
+    end
+  end
+  ranges.map {|k,v| k == v ? "#{k}" : "#{k}->#{v}"}
+end
